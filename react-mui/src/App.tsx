7b48cc96d8a3ec6e607e879233bde06e50e13816
rudaks <rudaks94@gmail.com>
2024-04-04 20:19:10 +0900

    update

---
+++ react-mui/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { MuiTypography } from "./components/MuiTypography.tsx";
+import { MuiButton } from "./components/MuiButton.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<MuiTypography />*/}
+      <MuiButton />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-mui/src/components/MuiTypography.tsx
@@ -0,0 +1,36 @@
+import Typography from "@mui/material/Typography";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>
+        h4 Heading
+      </Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">Sub title 1</Typography>
+      <Typography variant="subtitle2">Sub title 2</Typography>
+
+      <Typography variant="body1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Typography>
+      <Typography variant="body2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Typography>
+    </div>
+  );
+};
